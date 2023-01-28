329c2cd1440cfb90d2583bbc5f8f22773f3f6a79
SargeantK9 <bloodyredcoat@gmail.com>
2023-01-28 12:57:02 -0500

    font-size and fail
    changes font-size to rem; added funciton to prompt ratings selection if submit if clicked without rating being selected

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ let chosenRating;
 
 document.getElementById("submitButton").addEventListener(
   "click", () => {
-    document.getElementById("rating-state").style.display = "none";
-    document.getElementById("thankyou-state").style.display = "flex";
     detectRating();
 },
 false
 );
+
+function changeStates() {
+  document.getElementById("rating-state").style.display = "none";
+  document.getElementById("thankyou-state").style.display = "flex";
+}
 
 function detectRating() {
   const button1 = document.getElementById("button1");
@@ -18,18 +21,23 @@ function detectRating() {
   switch (true) {
     case button1.checked:
       chosenRating = 1;
+      changeStates();
       break;
     case button2.checked:
       chosenRating = 2;
+      changeStates();
       break;
     case button3.checked:
       chosenRating = 3;
+      changeStates();
       break;
     case button4.checked:
       chosenRating = 4;
+      changeStates();
       break;
     case button5.checked:
       chosenRating = 5;
+      changeStates();
       break;
     default:
       document.getElementById("heading1").innerHTML = "Please select a rating."
